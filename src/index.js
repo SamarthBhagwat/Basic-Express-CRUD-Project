b4dacca77371701772c2bb050ef93870def65f59
Samarth <samarthrn19@gmail.com>
2023-06-27 19:59:31 +0530

    Added home / route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ const {PORT} = require('./config/')
 
 const app = express()
 
+app.get('/', (req, res)=>{
+    return res.json({
+        'success': true,
+        'msg': 'Home route',
+        'data': {},
+        'error': {} 
+    })
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is up and running in port ${PORT}`)
 })
