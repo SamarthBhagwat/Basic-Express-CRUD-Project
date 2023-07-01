46ab2d5df8882b070c5b54ec1ebef22ee4020e8f
Samarth <samarthrn19@gmail.com>
2023-07-01 23:08:55 +0530

    Removed logger call on server startup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {PORT, logger} = require('./config/')
+const {PORT} = require('./config/')
 const apiRoutes = require('./routes')
 
 const app = express()
@@ -8,5 +8,4 @@ app.use('/api' , apiRoutes)
 
 app.listen(PORT, () =>{
     console.log(`Server is up and running in port ${PORT}`);
-    logger.info("Successfully started the server")
 })
