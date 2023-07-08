dc75f694ed130d16256fd3b574853c3b3c37d151
Samarth <samarthrn19@gmail.com>
2023-07-08 11:58:31 +0530

    Added middlewares for validating request

---
+++ src/middleware/airplane_middlewares.js
@@ -0,0 +1,20 @@
+const {StatusCodes} = require('http-status-codes')
+
+function validateCreateRequest(req, res, next){
+    if(!req.body.modelNumber){
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: "Something went wrong while creating airplane",
+            data: {},
+            error: {
+                explanation: "Model number was not found in the incoming request in the correct form"
+            }
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validateCreateRequest
+}
--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { AirplaneController } = require('../../controller');
+const { AirplaneMiddlewares } = require('../../middleware')
 const router = express.Router();
 
-router.post('/', AirplaneController.createAirplane);
+router.post('/', 
+            AirplaneMiddlewares.validateCreateRequest,
+            AirplaneController.createAirplane);
 
 module.exports = router; 
